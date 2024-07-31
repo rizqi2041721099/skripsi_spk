00f20b810a0a76beb21c4fe4794a9b89002cebe3
rizqi2041721099 <rizqiahmad939@gmail.com>
2024-07-31 20:28:36 +0700

    Fix:
    Fix bug jamOperasionalSeeder

--- database/seeders/KategoriJamOperasionalSeeder.php
+++ database/seeders/KategoriJamOperasionalSeeder.php
@@ -21,17 +21,17 @@ class KategoriJamOperasionalSeeder extends Seeder
                 'range_value' => '4 jam x < 8 jam',
             ],
             [
-                'value' => 5,
+                'value' => 3,
                 'standard_value' => 'Skala 3',
                 'range_value' => '8 jam <= x < 12 jam',
             ],
             [
-                'value' => 5,
+                'value' => 2,
                 'standard_value' => 'Skala 1',
                 'range_value' => '12 jam <= x < 16 jam',
             ],
             [
-                'value' => 5,
+                'value' => 1,
                 'standard_value' => 'Skala 1',
                 'range_value' => 'x < 4 jam',
             ],
